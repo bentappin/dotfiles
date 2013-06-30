660464e6fbcda5ea88050316e65a94162be86b5b
Ben Tappin <ben@mrben.co.uk>
2013-06-30 16:47:38 +0100

    Add 1pass.

--- bash/env.bash
+++ bash/env.bash
@@ -3,3 +3,7 @@ export CLICOLOR=1
 
 # Tell grep to highlight matches
 export GREP_OPTIONS='--color=auto'
+
+# Tell 1pass where the keychain is
+export ONEPASSWORD_KEYCHAIN=~/Dropbox/Personal\ Shared/1Password.agilekeychain
+
--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,5 @@
 brew install python --framework
 easy_install pip
-pip install virtualenv virtualenvwrapper
+pip install virtualenv virtualenvwrapper 1pass
 
 exit 0
