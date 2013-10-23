c44e0e26c198cbf3612f7ba6f389be9b971915a2
Ben Tappin <ben@mrben.co.uk>
2013-10-23 13:08:56 +0100

    Fix git completion.

--- git/completion.bash
+++ git/completion.bash
@@ -1,6 +1,12 @@
-completion="`brew --prefix`/share/zsh/site-functions/git-completion.bash"
+completion="`brew --prefix`/etc/bash_completion.d/git-completion.bash"
+prompt_completion="`brew --prefix`/etc/bash_completion.d/git-prompt.sh"
 
 if [ -f $completion ]
 then
 	source $completion
+fi
+
+if [ -f $prompt_completion ]
+then
+	source $prompt_completion
 fi
