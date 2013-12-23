befa82d64d480ff4dd8df480187b4cda0031235a
Ben Tappin <ben@mrben.co.uk>
2013-12-23 17:13:00 +0000

    Add colors() to bash aliases.

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -4,6 +4,18 @@ alias g=git
 alias v=vagrant
 alias ls="ls -Gp"
 
+colors() {
+  echo -e "\033[0mCOLOR_NC (No color)"
+  echo -e "\033[1;37mCOLOR_WHITE\t\033[0;30mCOLOR_BLACK"
+  echo -e "\033[0;34mCOLOR_BLUE\t\033[1;34mCOLOR_LIGHT_BLUE"
+  echo -e "\033[0;32mCOLOR_GREEN\t\033[1;32mCOLOR_LIGHT_GREEN"
+  echo -e "\033[0;36mCOLOR_CYAN\t\033[1;36mCOLOR_LIGHT_CYAN"
+  echo -e "\033[0;31mCOLOR_RED\t\033[1;31mCOLOR_LIGHT_RED"
+  echo -e "\033[0;35mCOLOR_PURPLE\t\033[1;35mCOLOR_LIGHT_PURPLE"
+  echo -e "\033[0;33mCOLOR_YELLOW\t\033[1;33mCOLOR_LIGHT_YELLOW"
+  echo -e "\033[1;30mCOLOR_GRAY\t\033[0;37mCOLOR_LIGHT_GRAY"
+}
+
 ord() {
   LC_CTYPE=C printf '%d' "'$1"
 }
@@ -15,8 +27,8 @@ pman() { man -t "$@" | open -f -a Preview; }
 flipcoin() { [[ $(( $RANDOM % 2 )) == 0 ]] && echo heads || echo tails; }
 
 source_file() {
-    if [ -f $1 ];
-    then
-       source $1
-    fi
+  if [ -f $1 ];
+  then
+     source $1
+  fi
 }
