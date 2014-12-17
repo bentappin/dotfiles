848c4e5cafe963874c318333cbb49d3dc5a8202c
Ben Tappin <ben@mrben.co.uk>
2014-12-17 14:25:03 +0000

    Set hostname from ~/.hostname

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -29,10 +29,10 @@
 ###############################################################################
 
 # Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "apfel"
-sudo scutil --set HostName "apfel"
-sudo scutil --set LocalHostName "apfel"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "apfel"
+sudo scutil --set ComputerName "$(< ~/.hostname)"
+sudo scutil --set HostName "$(< ~/.hostname)"
+sudo scutil --set LocalHostName "$(< ~/.hostname)"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$(< ~/.hostname)"
 
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
