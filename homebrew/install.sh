b81e6e5dd8898e46a765691828c996b1358678d2
Ben Tappin <ben@mrben.co.uk>
2014-12-17 14:35:19 +0000

    Clean up homebrew and cask installs.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,33 +12,28 @@ fi
 
 # Install homebrew packages
 # First install some libs that may or may not need to be installed before other things...
-brew install gcc flac lame libjpeg swig freetype watch
+brew install gcc flac lame libjpeg little-cms2 swig freetype watch wget
 # Now install some "bigger" things...
-brew install ffmpeg git gnupg grc libsndfile lynx mercurial openssl postgresql rabbitmq wget
+brew install ffmpeg git gnupg grc libsndfile lynx openssl postgresql rabbitmq mp3gain mp3info youtube-dl p7zip cdrtools
 
 brew install gettext && brew link gettext --force
 
 # Install brew-cask for binary installs
-brew tap caskroom/cask
 brew tap caskroom/versions
-brew install brew-cask
+brew install caskroom/cask/brew-cask
 
 # Install apps to /Applications
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # Install native apps
 brew cask install alfred
-brew cask install colors
 brew cask install dropbox
 brew cask install firefox
 brew cask install flux
 brew cask install google-chrome
-brew cask install handbrake
-brew cask install imageoptim
-brew cask install iterm2-beta
+brew cask install iterm2
 brew cask install launchrocket
 brew cask install lightpaper
-brew cask install limechat
 brew cask install onepassword
 brew cask install skype
 brew cask install sublime-text-dev
@@ -46,16 +41,5 @@ brew cask install transmission
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install vlc
-
-# Download the precise vagrant box in advance if user wishes.
-echo "Do you want to add the precise vagrant box now? (It could take 30 minutes.) [y/N]:"
-read -t 10 add_precise
-
-if [ "$add_precise" == "y" ] || [ "$add_precise" == "Y" ]; then
-  echo "Adding precise32 vagrant box..."
-  vagrant box add precise32 http://files.vagrantup.com/precise32.box
-else
-  echo "Skipping..."
-fi
 
 exit 0
