2321f4f1944fb0b75b0cbfb4a40b5f818636e75c
Ben Tappin <ben@mrben.co.uk>
2016-03-30 22:18:27 +0100

    Add GPG key for rvm install.

--- rvm/install.sh
+++ rvm/install.sh
@@ -7,6 +7,7 @@
 # Check for RVM, install if not present
 if [ -z `which rvm` ];
 then
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable
 fi
 
