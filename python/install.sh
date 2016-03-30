b8dd5aa58f8ae147be52b8da5d12e349c3d500e2
Ben Tappin <ben@mrben.co.uk>
2016-03-30 22:46:31 +0100

    Fix pyenv install.

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,8 @@
 brew install pyenv
 
+export PYENV_ROOT=/usr/local/var/pyenv
+eval "$(pyenv init -)"
+
 pyenv install `pyenv install --list | grep "  2.7" | tail -n 1 | xargs`
 pyenv install `pyenv install --list | grep "  3.4" | tail -n 1 | xargs`
 pyenv install `pyenv install --list | grep "  3.5" | tail -n 1 | xargs`
