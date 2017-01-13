fac4184ed2eebd70b42744fcddc335a841cd9060
Ben Tappin <ben@mrben.co.uk>
2017-01-13 18:42:24 +0000

    Don't install any dev Python versions.

--- python/install.sh
+++ python/install.sh
@@ -4,9 +4,9 @@ brew install pyenv pyenv-virtualenv pyenv-virtualenvwrapper
 export PYENV_ROOT=/usr/local/var/pyenv
 eval "$(pyenv init -)"
 
-pyenv install `pyenv install --list | grep "  2.7" | tail -n 1 | xargs`
-pyenv install `pyenv install --list | grep "  3.4" | tail -n 1 | xargs`
-pyenv install `pyenv install --list | grep "  3.5" | tail -n 1 | xargs`
-pyenv install `pyenv install --list | grep "  3.6" | tail -n 1 | xargs`
+pyenv install `pyenv install --list | grep "  2.7" | grep -v "dev" | tail -n 1 | xargs`
+pyenv install `pyenv install --list | grep "  3.4" | grep -v "dev" | tail -n 1 | xargs`
+pyenv install `pyenv install --list | grep "  3.5" | grep -v "dev" | tail -n 1 | xargs`
+pyenv install `pyenv install --list | grep "  3.6" | grep -v "dev" | tail -n 1 | xargs`
 
 exit 0
