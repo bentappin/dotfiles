6685d43bd4dfa6a28fe2fa2eb8783df3801e8f6c
Ben Tappin <ben@mrben.co.uk>
2017-01-13 18:48:57 +0000

    Don't restart Terminal in macOS script otherwise we can't run rest of the stuffs.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -725,7 +725,7 @@ defaults write org.m0k.transmission WarningLegal -bool false
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
 	"Dock" "Finder" "Google Chrome" "Mail" "Messages" \
-	"Opera" "Photos" "Safari" "SystemUIServer" "Terminal" "Transmission" "iCal"; do
+	"Opera" "Photos" "Safari" "SystemUIServer" "Transmission" "iCal"; do
 	killall "${app}" &> /dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
