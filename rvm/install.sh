1beb647700530c2c88ad0dd2b71559e37358e72d
Ben Tappin <ben@mrben.co.uk>
2017-01-13 14:55:35 +0000

    Install ruby when installing rvm.

--- rvm/install.sh
+++ rvm/install.sh
@@ -8,7 +8,7 @@
 if [ -z `which rvm` ];
 then
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  curl -sSL https://get.rvm.io | bash -s stable
+  curl -sSL https://get.rvm.io | bash -s stable --ruby
 fi
 
 rvm_source="$HOME/.rvm/scripts/rvm"
