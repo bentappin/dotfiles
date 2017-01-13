f25d40a18d316ad5b0fde1950724fc7f537fc7cf
Ben Tappin <ben@mrben.co.uk>
2017-01-13 13:30:39 +0000

    Update Python versions.

--- python/install.sh
+++ python/install.sh
@@ -1,3 +1,4 @@
+brew install python3
 brew install pyenv pyenv-virtualenv pyenv-virtualenvwrapper
 
 export PYENV_ROOT=/usr/local/var/pyenv
@@ -6,5 +7,6 @@ eval "$(pyenv init -)"
 pyenv install `pyenv install --list | grep "  2.7" | tail -n 1 | xargs`
 pyenv install `pyenv install --list | grep "  3.4" | tail -n 1 | xargs`
 pyenv install `pyenv install --list | grep "  3.5" | tail -n 1 | xargs`
+pyenv install `pyenv install --list | grep "  3.6" | tail -n 1 | xargs`
 
 exit 0
