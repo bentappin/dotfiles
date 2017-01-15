b8ecbc9902389d89eda368545d8634375f124f02
Ben Tappin <ben@mrben.co.uk>
2017-01-15 19:41:50 +0000

    Add disable-pyenv function.

--- python/env.bash
+++ python/env.bash
@@ -4,3 +4,8 @@ if which pyenv-virtualenv-init > /dev/null; then
 	eval "$(pyenv virtualenv-init -)"
 	export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 fi
+
+function disable-pyenv {
+	# Remove anything with 'pyenv' in from PATH.
+	export PATH=$(p=$(echo $PATH | tr ":" "\n" | grep -v "pyenv" | tr "\n" ":"); echo ${p%:})
+}
